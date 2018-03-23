7652790be50c01643b847284e28757a7666a258d
Dmitry <dsuschinsky@gmail.com>
2018-03-23 15:23:08 +0300

    add Answer controller

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :question do
-    title "MyString"
-    body "MyText"
+    title 'MyString'
+    body 'MyText'
   end
 
-  factory :invalid_question, class: "Question" do
+  factory :invalid_question, class: 'Question' do
     title nil
     body nil
   end
