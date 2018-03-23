ed1e3d4678ad4165fe8643c323438b7b61fa4879
Dmitry <dsuschinsky@gmail.com>
2018-03-23 10:36:59 +0300

    fix issues

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe Answer do
-  
-    context 'validation' do
-      it { should validate_presence_of :body }
-      it { should validate_length_of(:body).is_at_least 1 }
-    end
+
+  context 'validation' do
+    it { should validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least 1 }
+  end
 
   context 'association' do
     it { should belong_to :question }
