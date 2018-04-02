7f94cac85d267381bed6b6cd2537847425867589
Dmitry <dsuschinsky@gmail.com>
2018-04-02 11:19:07 +0300

    make attachment to polymorphic

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
-  has_many :attachments, dependent: :destroy
+  has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { minimum: 5 }
