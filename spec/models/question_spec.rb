40864f018bd077f42af3e54a55fa86b438a234fe
Dmitry <dsuschinsky@gmail.com>
2018-04-02 11:13:35 +0300

    Attachment model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ describe Question do
   describe 'association' do
     it { should have_many(:answers).dependent :destroy }
     it { should belong_to :user }
+    it { should have_many(:attachments).dependent :destroy }
   end
 
   context 'validation' do
@@ -11,5 +12,9 @@ describe Question do
     it { should validate_presence_of :body }
     it { should validate_length_of(:title).is_at_least 3 }
     it { should validate_length_of(:body).is_at_least 5 }
+  end
+
+  describe 'attributes' do
+    it { should accept_nested_attributes_for :attachments }
   end
 end
