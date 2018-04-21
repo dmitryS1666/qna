ce60aebfc0470a854565c07aede97b1f6e240889
dmitryS1666 <dsuschinsky@gmail.com>
2018-04-21 21:45:35 +0100

    Change gem file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require skim
+// require skim
 //= require_tree ./templates
 //= require rails-ujs
 //= require turbolinks
