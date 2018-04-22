f5670cc57de33c734c6c065b15c21ae6c3eba038
dmitryS1666 <dsuschinsky@gmail.com>
2018-04-22 21:45:50 +0100

    Add Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.2"
+
+set :application, "qna"
+set :repo_url, "git@github.com:dmitryS1666/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, "deployer"
+set :format_options, log_file: "log/capistrano.log"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml"
+append :linked_files, ".env"
+# append :linked_files, "public/system"
+# append :linked_files, "public/uploads"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, realese_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
