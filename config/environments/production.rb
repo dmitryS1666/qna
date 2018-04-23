123450bb8cfcaecdd8c954424b4f5fbc5e8731fa
Dmitry <dsuschinsky@gmail.com>
2018-04-23 18:07:27 +0300

    fix issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,16 +89,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => '46.21.249.120' }
-  # config.action_mailer.only_path = true
-  config.action_mailer.raise_delivery_errors = true
+  Rails.application.routes.default_url_options[:host] = 'telepathica.ru'
+  config.action_cable.allowed_request_origins = ["46.21.249.120"]
+  config.action_mailer.default_url_options = { host: '46.21.249.120', port: '4321' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => '46.21.249.120',
-      :user_name            => ENV['GMAIL_USERNAME'],
-      :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => 'plain',
-      :enable_starttls_auto => true  }
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: '46.21.249.120',
+      authentication: 'plain',
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD'],
+      enable_starttls_auto: true
+  }
 end
